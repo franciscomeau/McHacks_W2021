d0e5f9a326fa4a492c1f722657b1b780c5bf5f5c
affanjilani <obiaffan@gmail.com>
2021-01-30 12:07:54 -0500

    changed to fetcfh

--- frontend/popup.js
+++ frontend/popup.js
@@ -10,12 +10,17 @@ function getURL() {
 };
 
 function getIngredientSeasons(url) {
-    axios.post('https://mchacks-2k21.herokuapp.com/',{url: url})
-        .then(response => {
-            console.log('After fetch!', response, response.data)
-            addIngredientsToUI(response.data);
-        })
-        .catch(error => console.error(error));
+    fetch('https://mchacks-2k21.herokuapp.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({url: url})
+    }).then(response => {
+        console.log('fetched!', response, response.json())
+    }).catch(error => {
+        console.error(error);
+    });
 }
 
 function addIngredientsToUI(ingredientSeasons) {
