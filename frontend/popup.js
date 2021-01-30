6955354aec4f826df1b677dc2a952a4ce064773a
johndwight <j_paraga@live.concordia.ca>
2021-01-30 14:17:23 -0500

    printIngredients

--- frontend/popup.js
+++ frontend/popup.js
@@ -36,17 +36,32 @@ function addIngredientsToUI(ingredientSeasons) {
 		console.log(ingredientSeasons[key].in_season);
 		if(ingredientSeasons[key].in_season == "In season"){
 			// Print out in the inSeason div
+			printIngredients(ingredientSeasons[key].ingredient, 'in_season');
+			
 
 		} else if(ingredientSeasons[key].in_season == "Not in season"){
 			// Print out in the not_in_season div
+			printIngredients(ingredientSeasons[key].ingredient, 'not_in_season');
+
 
 		} else if(ingredientSeasons[key].in_season == "N/A"){
 			// Print out in the other div
-			
+			printIngredients(ingredientSeasons[key].ingredient, 'other');
+
 		}
 	}
 
 	
+}
+
+function printIngredients(anIngredient, divID){
+	let seasonality = document.getElementById(divID);
+
+	let ingredientNode = document.createElement('p');
+	let ingredientNodeText = document.createTextNode(anIngredient);
+	ingredientNode.appendChild(ingredientNodeText);
+	seasonality.appendChild(ingredientNode);
+
 }
 
 
