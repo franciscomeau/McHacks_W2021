145209cda3dd001a42ce72b5033e7ec6c09a3b58
affanjilani <obiaffan@gmail.com>
2021-01-30 11:52:51 -0500

    Added backend fetching

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,10 +1,25 @@
 function getURL() {
-	
+	let url;
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	     var activeTab = tabs[0];
-	     var myUrl = activeTab.url;
-	     console.log(myUrl);
-	  });	
+	     url = activeTab.url;
+	     console.log(url);
+      });
+      
+    getIngredientSeasons(url);
 };
+
+function getIngredientSeasons(url) {
+    axios.post('https://mchacks-2k21.herokuapp.com/',{url: url})
+        .then(response => {
+            console.log('After fetch!', response, response.data)
+            addIngredientsToUI(response.data);
+        })
+        .catch(error => console.error(error));
+}
+
+function addIngredientsToUI(ingredientSeasons) {
+    //DO DOM shit
+}
 
 document.getElementById('startApp').addEventListener('click', getURL);
