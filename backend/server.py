c14db26141ea7ff137ae92a88a82b1de3ce5ae95
mattjliu <matthew.liu041296@gmail.com>
2021-01-30 12:29:59 -0500

    print request

--- backend/server.py
+++ backend/server.py
@@ -4,15 +4,17 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST'])
+
+@app.route("/", methods=['POST'])
 def get_seasonal_details():
+    print(request)
     url = request.get_json()['url']
     ingredients = get_ingredients_from_url(url)
     in_season_dict = check_seasonality(ingredients, 1)
 
     print(in_season_dict)
-    
-    return json.dumps(in_season_dict), 200, {'ContentType':'application/json'}
+
+    return json.dumps(in_season_dict), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
