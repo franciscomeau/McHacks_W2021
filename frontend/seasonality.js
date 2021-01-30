7c079d6b80cbc2018aea075352d5066e09cfafa0
johndwight <j_paraga@live.concordia.ca>
2021-01-30 10:25:39 -0500

    insert element logic

--- frontend/seasonality.js
+++ frontend/seasonality.js
@@ -1,8 +1,19 @@
+let count = 0;
+
+
 function printIngredients(){
-    let inSeason = document.getElementById('in_season');
-    let notInSeason = document.getElementById('not_in_season');
-    console.log(inSeason);
+    if(count == 0){
+        let inSeason = document.getElementById('in_season');
+        let notInSeason = document.getElementById('not_in_season');
+        let other = document.getElementById('other');
+    
+        let ingredientNode = document.createElement('p');
+        let ingredientNodeText = document.createTextNode("Blablabla");
+        ingredientNode.appendChild(ingredientNodeText);
+        inSeason.appendChild(ingredientNode);
+        count++;
+    }
 }
 
-document.getElementById('in_season').addEventListener('click', printIngredients);
+document.getElementById('startApp').addEventListener('click', printIngredients);
 
