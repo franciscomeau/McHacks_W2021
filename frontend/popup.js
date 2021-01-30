019463020e0f0b5add0342593fe1ae71c08bc20e
franciscomeau <francis.comeau@mail.mcgill.ca>
2021-01-30 11:13:14 -0500

    parse exmaple working

--- frontend/popup.js
+++ frontend/popup.js
@@ -4,7 +4,13 @@ function getURL() {
 	     var activeTab = tabs[0];
 	     var myUrl = activeTab.url;
 	     console.log(myUrl);
-	  });	
+	  });
+	
+	var request = new XMLHttpRequest();
+    request.open("GET", "sample_ingredients.json", false);
+    request.send(null);
+    var my_JSON_object = JSON.parse(request.responseText);
+    console.log(JSON.stringify(my_JSON_object));
 };
 
 document.getElementById('startApp').addEventListener('click', getURL);
