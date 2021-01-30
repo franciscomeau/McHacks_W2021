fb7677025cc7c119871319571226ee9094ff0fd9
affanjilani <obiaffan@gmail.com>
2021-01-30 00:24:17 -0500

    Added post with json

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,18 @@
 from flask import Flask, request
 from ingredient_matcher import check_seasonality, get_ingredients_from_url
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods = ['POST'])
 def get_seasonal_details():
-    url = request.args.get('url')
-    check_seasonality()
-    if not url:
-        url = "Must provide a url"
-    return url
+    url = request.get_json()['url']
+    ingredients = get_ingredients_from_url(url)
+    in_season_dict = check_seasonality(ingredients, 1)
+
+    print(in_season_dict)
+    
+    return json.dumps(in_season_dict), 200, {'ContentType':'application/json'}
 
 
 if __name__ == '__main__':
